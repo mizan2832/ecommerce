299fccad53e5865f910f464f9873d324ee935cff
mizan2832 <programmer620@gmail.com>
2021-11-19 18:21:40 +0600

    templating

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -8,13 +8,13 @@
     <link rel="shortcut icon" type="image/x-icon" href="front/assets/images/favicon.ico">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/animate.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/font-awesome.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/bootstrap.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/chosen.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/style.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('front/front/assets/css/color-01.css')}} ">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/animate.css ">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/font-awesome.min.css ">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/bootstrap.min.css ">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/chosen.min.css ">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="front/assets/css/color-01.css">
 </head>
 <body class="home-page home-01 ">
 
